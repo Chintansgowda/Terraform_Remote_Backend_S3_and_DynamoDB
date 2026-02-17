4de2188e68493e54ebfbb0c6de5038c80cfaf11e
Chintan_06 <159480965+Chintansgowda@users.noreply.github.com>
2026-02-17 20:40:32 +0530

    Fix formatting in backend.tf for Terraform configuration

--- backend.tf
+++ backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "chinatn-06-s3-demo-2323" 
+    bucket         = "chintan-06-s3-demo-2323" 
     key            = "chintan/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
 }
+
 
