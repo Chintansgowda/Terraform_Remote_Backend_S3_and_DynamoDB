1134a1e594e4c895c6b40136a3c1ad2f57280a76
Chintan_06 <159480965+Chintansgowda@users.noreply.github.com>
2026-02-17 20:38:37 +0530

    Update S3 backend configuration in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
+    bucket         = "chinatn-06-s3-demo-2323" 
+    key            = "chintan/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
 }
+
